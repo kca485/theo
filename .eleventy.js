8528522f6ec5d7266caf2a82ee6218af4968ce4b
Karntino Areros <karntino.c.areros@gmail.com>
2022-09-12 16:43:05 +0700

    intial commit

---
+++ .eleventy.js
@@ -0,0 +1,10 @@
+module.exports = function(eleventyConfig) {
+  eleventyConfig.addPassthroughCopy('src/scripts');
+  eleventyConfig.addPassthroughCopy('src/admin');
+
+  return {
+    dir: {
+      input: 'src'
+    }
+  };
+}
