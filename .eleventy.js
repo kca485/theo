da595116e55f86a00a072a7509ff1391cea38dea
Karntino Areros <karntino.c.areros@gmail.com>
2022-09-30 21:14:41 +0700

    add images directory to prod

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,7 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy('src/scripts');
   eleventyConfig.addPassthroughCopy('src/styles');
   eleventyConfig.addPassthroughCopy('src/admin');
+  eleventyConfig.addPassthroughCopy('src/images');
 
   return {
     dir: {
